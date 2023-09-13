a0c353f83679b4465a82cacf7ad3a3923680f91e
Talia Hurrell <44178314+taliafr0st@users.noreply.github.com>
2023-09-13 19:59:52 +0100

    Discord Bot Setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+const auth = require('../auth/auth.json');
+
 const express = require('express');
-const cors = require('cors');
 const app = express();
+
+const cors = require('cors');
+
+const discord = require('discord.js');
+import ready from "./listeners/ready";
+const client = new discord.Client({
+    intents: []
+});
+ready(client);
+client.login(auth['discord-token'])
+//console.log(client);
 
 var listener = app.listen(3000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 3000
